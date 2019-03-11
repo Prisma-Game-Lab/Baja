08fb35916dc78a0c8848190962f39c499706a6b6
mlhomsi <mlhomsi@gmail.com>
2019-03-10 22:59:13 -0300

    Countdown & Checkpoints(wincondition)

---
+++ Baja/Assets/CheckPoint.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckPoint : MonoBehaviour {
+
+    public bool IsFinal;
+    public int ObstacleNumber;
+
+    [HideInInspector]
+    public GameObject FinalPanel;
+    [HideInInspector]
+    public Text FinalPanelText;
+    [HideInInspector]
+    public GameObject Countdown;
+
+    private float result;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if(other.GetComponent<Register>().HowMany == ObstacleNumber-1)
+            {
+                other.GetComponent<Register>().HowMany++;
+                //Debug.Log(ObstacleNumber);
+                if(IsFinal)
+                { Time.timeScale = 0f;
+                    result = Countdown.GetComponent<Countdown>().laptime;
+                    FinalPanelText.text = "Time Left: " + Mathf.Round(result);
+                    FinalPanel.SetActive(true);
+                }
+            }
+        }
+    }
+}
---
+++ Baja/Assets/Register.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Register : MonoBehaviour {
+
+    public int HowMany;
+
+    private void Start()
+    {
+        HowMany = 0;
+    }
+}
