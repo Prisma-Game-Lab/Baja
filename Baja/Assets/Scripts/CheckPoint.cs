8327e840ee2308a6d93dfd7dbce42ec2f3bbbfd3
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-06-02 16:33:25 -0300

    Prefabs UI e CheckPoint

--- Baja/Assets/Scripts/CheckPoint.cs
+++ Baja/Assets/Scripts/CheckPoint.cs
@@ -5,9 +5,9 @@ using UnityEngine.UI;
 
 public class CheckPoint : MonoBehaviour {
 
-    public bool IsFinal;
-    public bool Lap1;
-    public bool Lap2;
+    public bool FirstLap;
+    public bool SecondLap;
+    public bool FinalLap;
     public int ObstacleNumber;
     public GameObject gLap;
 
@@ -16,9 +16,9 @@ public class CheckPoint : MonoBehaviour {
     [HideInInspector]
     public Text FinalPanelText;
 
-    private float result1;
-    private float result2;
-    private float result3;
+    private static float ResultFirstLap;
+    private static float ResultSecondLap;
+    private static float ResultFinalLap;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -27,26 +27,26 @@ public class CheckPoint : MonoBehaviour {
             if(other.GetComponent<Register>().HowMany == ObstacleNumber-1)
             {
                 other.GetComponent<Register>().HowMany++;
-                if(Lap1)
+                if(FirstLap) // Completou o primeiro lap
                 {
-                    result1 = Countdown.laptime;
+                    ResultFirstLap = Countdown.laptime; // Resultado primero lap
                     other.GetComponent<Register>().HowMany = 0;
                     gLap.SetActive(true);
                     this.gameObject.SetActive(false);
                 }
-                if(Lap2)
+                if(SecondLap) // Completou o segundo lap
                 {
-                    result2 = Countdown.laptime;
+                    ResultSecondLap = Countdown.laptime; // Resultado segundo lap
                     other.GetComponent<Register>().HowMany = 0;
                     gLap.SetActive(true);
                     this.gameObject.SetActive(false);
                 }
-                if(IsFinal)
+                if(FinalLap) // Completou o terceiro lap
                 { Time.timeScale = 0f;
-                    result3 = Countdown.laptime;
-                    FinalPanelText.text = "\n\nLap1: " + (Mathf.Round(result1*1000f)/1000f) + "\n" +
-                                          "Lap2: " + (Mathf.Round(result2*1000f)/1000f) + "\n" +
-                                          "Lap3: " + (Mathf.Round(result3*1000f)/1000f);
+                    ResultFinalLap = Countdown.laptime; // Resultado terceiro lap
+                    FinalPanelText.text = "\n\nLap1: " + (Mathf.Round(ResultFirstLap*1000f)/1000f) + "\n" +
+                                          "Lap2: " + (Mathf.Round(ResultSecondLap*1000f)/1000f) + "\n" +
+                                          "Lap3: " + (Mathf.Round(ResultFinalLap*1000f)/1000f);
                     FinalPanel.SetActive(true);
                 }
             }
