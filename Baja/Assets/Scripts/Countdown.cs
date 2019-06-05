97b99affa59e93944dcfe0487f2509a67080aa2a
ViniciusCortat <31554254+ViniciusCortat@users.noreply.github.com>
2019-06-05 20:24:32 -0300

    Update Countdown.cs

--- Baja/Assets/Scripts/Countdown.cs
+++ Baja/Assets/Scripts/Countdown.cs
@@ -5,16 +5,28 @@ using UnityEngine.UI;
 
 public class Countdown : MonoBehaviour {
     public Text timer;
+
+	public GameObject CheckPoint;
     public static float laptime = 0;
 	// Use this for initialization
 	void Start () {
         laptime = -0.02f;
-        timer.text = "Timer: ";
+        timer.text = "Timer: ";	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        timer.text = "Timer: " + (Mathf.Round(laptime*1000f)/1000f);
+void Update () {
+        timer.text = "Timer: " + (Mathf.Round(laptime*1000f)/1000f) + "\nLap: ";
+		if(CheckPoint.transform.GetChild(2).gameObject.activeSelf) {
+			timer.text += "1";
+		}
+		if(CheckPoint.transform.GetChild(3).gameObject.activeSelf) {
+			timer.text += "2";
+		}
+		if(CheckPoint.transform.GetChild(4).gameObject.activeSelf) {
+			timer.text += "3";
+		}
         laptime += Time.deltaTime;
 	}
 }
+
