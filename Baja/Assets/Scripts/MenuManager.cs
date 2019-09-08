95f46d6deedd72c0c8c4a35284c5c4f0924dcb47
Hugo Tonette <hugo.tonette@gmail.com>
2019-09-08 20:33:21 -0300

    verifica se tem algum controle conectado, pra mostrar o tutorial certo

--- Baja/Assets/Scripts/MenuManager.cs
+++ Baja/Assets/Scripts/MenuManager.cs
@@ -1,102 +1,110 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuManager : MonoBehaviour {
+public class MenuManager : MonoBehaviour
+{
+	public GameObject StartMenu;
+	public GameObject PauseMenu;
+	public GameObject OptionMenu;
+	public GameObject Timer;
+	public GameObject WinPanel;
+	public GameObject ControllerInstructions;
+	public GameObject KeyboardInstructions;
+	public GameObject HUD;
+	[HideInInspector]
+	public bool IsPaused = true;
+	public static bool AfterCountdown = false;
 
-public GameObject StartMenu;
-    public GameObject PauseMenu;
-    public GameObject OptionMenu;
-    public GameObject Timer;
-    public GameObject WinPanel;
-    public GameObject TutorialPanel;
-    public GameObject HUD;
-
-    public bool IsPaused = true;
-    public static bool AfterCountdown = false;
-
-
-	// Use this for initialization
-	void Start () {
-        Time.timeScale = 0.0f;
-        StartMenu.SetActive(true);
-        PauseMenu.SetActive(false);
-        OptionMenu.SetActive(false);
-        WinPanel.SetActive(false);
-        Timer.SetActive(false);
-        Countdown.laptime = -0.02f;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if(Input.GetButtonDown("Pause"))
-        {
-            PauseGame();
-        }
-
+	void Start()
+	{
+		Time.timeScale = 0.0f;
+		StartMenu.SetActive(true);
+		PauseMenu.SetActive(false);
+		OptionMenu.SetActive(false);
+		WinPanel.SetActive(false);
+		Timer.SetActive(false);
+		Countdown.laptime = -0.02f;
 	}
 
-    public void StartGame ()
-    {
-        TutorialPanel.SetActive(false);
-        HUD.SetActive(true);
-        IsPaused = false;
-        Timer.SetActive(true);
-        Time.timeScale = 1.0f;
-        Countdown.laptime = -0.02f;
-        AfterCountdown = true;
-    }
+	void Update()
+	{
+		if (Input.GetButtonDown("Pause"))
+		{
+			PauseGame();
+		}
+	}
 
-    public void OpenInstructions(){
-        StartMenu.SetActive(false);
-        WinPanel.SetActive(false);
-        TutorialPanel.SetActive(true);
-    }
+	public void StartGame()
+	{
+		//TutorialPanel.SetActive(false);
+		CheckInputMethod(false);
+		HUD.SetActive(true);
+		IsPaused = false;
+		Timer.SetActive(true);
+		Time.timeScale = 1.0f;
+		Countdown.laptime = -0.02f;
+		AfterCountdown = true;
+	}
+
+	public void OpenInstructions()
+	{
+		StartMenu.SetActive(false);
+		WinPanel.SetActive(false);
+		CheckInputMethod(true);
+		//TutorialPanel.SetActive(true);
+	}
 
 
-    private void PauseGame()
-    {
-        if (IsPaused == true)
-        { 
-            Time.timeScale = 1.0f;
-            PauseMenu.SetActive(false);
-            IsPaused = false;
-        }
-        else if(!WinPanel.activeInHierarchy && !OptionMenu.activeInHierarchy && !StartMenu.activeInHierarchy)
-        {
-            Time.timeScale = 0.0f;
-            PauseMenu.SetActive(true);
-            IsPaused = true;
-        }
+	private void PauseGame()
+	{
+		if (IsPaused == true)
+		{
+			Time.timeScale = 1.0f;
+			PauseMenu.SetActive(false);
+			IsPaused = false;
+		}
+		else if (!WinPanel.activeInHierarchy && !OptionMenu.activeInHierarchy && !StartMenu.activeInHierarchy)
+		{
+			Time.timeScale = 0.0f;
+			PauseMenu.SetActive(true);
+			IsPaused = true;
+		}
+	}
 
-    }
+	public void OptionsMenu()
+	{
+		OptionMenu.SetActive(true);
+		StartMenu.SetActive(false);
+	}
 
-    public void OptionsMenu()
-    {
-        OptionMenu.SetActive(true);
-        StartMenu.SetActive(false);
-    }
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("Quitei");
+	}
 
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quitei");
-    }
+	public void BackMenu()
+	{
+		OptionMenu.SetActive(false);
+		StartMenu.SetActive(true);
+	}
 
-    public void BackMenu()
-    {
-        OptionMenu.SetActive(false);
-        StartMenu.SetActive(true);
-    }
+	public void BackFromPause()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+	}
 
-    public void BackFromPause()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-    }
+	public void Continue()
+	{
+		PauseGame();
+	}
 
-    public void Continue()
-    {
-        PauseGame();
-    }
+	// Check to see if theres a controller conected or not
+	private void CheckInputMethod(bool state)
+	{
+		if (Input.GetJoystickNames().Length > 0)
+			ControllerInstructions.SetActive(state);
+		else
+			KeyboardInstructions.SetActive(state);
+	}
 }
