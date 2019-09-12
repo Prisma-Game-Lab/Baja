daea7a9e5d891f3ecf72ea76bf4c6f874bf91002
Renan da Fonte <renandafonte@hotmail.com>
2019-09-11 21:23:15 -0300

    input webgl

---
+++ Baja/Assets/Scripts/Debug/TestInput.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestInput : MonoBehaviour
+{
+    public string InputToTest;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(ControllerDetector.GetButtonDown(InputToTest) || ControllerDetector.GetButton(InputToTest)) Debug.Log(InputToTest + ": " + ControllerDetector.GetButtonDown(InputToTest));
+    }
+}
