68af54a3a17ed203ecf1402167d0294e1025aa59
Pedro Fernando <pferndd@>
2019-09-12 23:43:16 -0300

    Codigo particula

---
+++ Baja/Assets/Scripts/Effects/ParticleEmit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEmit : MonoBehaviour
+{
+    ParticleSystem particles;
+    void Start() {
+        particles = GetComponent<ParticleSystem>();
+    }
+    void Update()
+    {
+        /*if(CARRO TA SE MOVENDO){
+            particles.Play();
+        }else{
+            particles.Stop();
+        }*/
+    }
+}
